d96b88218c3516d062a25d2db2e01b74382efaa2
EugenGam <56554868+EugenGam@users.noreply.github.com>
2020-02-06 19:35:35 +0200

    Update task-6.js

--- Module-01/task-6.js
+++ Module-01/task-6.js
@@ -3,11 +3,8 @@ let inpt;
 
 do {
   inpt = prompt('Введите число');
-<<<<<<< HEAD
   const inp = Number(inpt);
-=======
   inp = Number(inpt);
->>>>>>> 75fa56064fe3e0a7997e71dbcb1189cde13d0a22
   console.log(typeof totl);
   totl += inp;
 } while (inpt != null);
